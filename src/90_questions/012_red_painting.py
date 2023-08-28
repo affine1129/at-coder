c8e736153305f6cd5b785eba7a2c67ba5598142b
affine1129 <affine1129@gmail.com>
2023-08-29 08:20:58 +0900

    Add 90-012(UF)

--- src/90_questions/012_red_painting.py
+++ src/90_questions/012_red_painting.py
@@ -1,50 +1,63 @@
 import sys
 
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 H, W = map(int, input().split())
 Q = int(input())
 
+class UF:
+    def __init__(self, h, w):
+        self.h = h+2
+        self.w = w+2
+        # 親ノードの時は負の値でサイズを持つ
+        self.par_or_size = [ 0 for _ in range(self.h*self.w)]
+    
+    def root(self, x):
+        p = x
+        while self.par_or_size[p] > 0:
+            p = self.par_or_size[p]
+        while self.par_or_size[x] > 0:
+            tmp = self.par_or_size[x]
+            self.par_or_size[x] = p
+            x = tmp
+        return p
 
-def dfs(dept, dest, mat, dir=[[0, 1], [0, -1], [1, 0], [-1, 0]]):
-    res = 0
-    if mat[dept[0]][dept[1]] == 1:
-        if dept == dest:
+    def size(self, x):
+        return -self.par_or_size[self.root(x)]
+
+    def unite(self, x):
+        self.par_or_size[x] = -1
+        for dir in [x-1, x+1, x+self.w, x-self.w]:
+            if self.par_or_size[dir] != 0:
+                x = self.root(x)
+                y = self.root(dir)
+                if x == y:
+                    return
+                if self.size(x) < self.size(y):
+                    x, y = y, x
+                self.par_or_size[x] += self.par_or_size[y]
+                self.par_or_size[y] = x
+
+    def is_same(self, x, y):
+        if self.root(x) == self.root(y) and self.par_or_size[x] != 0 and self.par_or_size[y] != 0:
             return True
-        for d in dir:
-            nex = [dept[0] + d[0], dept[1] + d[1]] 
-            if nex[0] > dest[0] or nex[1] > dest[0]:
-                continue
-            res += dfs(nex, dest, mat)
-    return bool(res)
+        else:
+            return False
 
-mat = [[0] * W for _ in range(H)]
+
+uf = UF(H, W)
 for _ in range(Q):
     t_r_c = list(map(int, input().split()))
     if t_r_c[0] == 1:
-        r, c = map(lambda x: x-1, t_r_c[1:])
-        mat[r][c] = 1
+        r, c = t_r_c[1:]
+        uf.unite(r*uf.w+c) 
 
     else:
-        ra, ca, rb, cb = map(lambda x: x-1, t_r_c[1:])
-        ans = False
-        # aが右下, bが左上
-        if ra > rb and ca > cb:
-            ans = dfs([rb, cb], [ra, ca], mat[rb:ra][cb:ca])
-        # aが左下, bが右上
-        elif ra > rb and cb > ca:
-            ans = dfs([H-1-ra, ca], [H-1-rb, cb], mat[::-1])
-        # aが右上, bが左下
-        elif rb > ra and ca > cb:
-            ans = dfs([H-1-rb, cb], [H-1-ra, ca], mat[::-1])
-        # その他
-        else:
-            ans = dfs([ra, ca], [rb, cb], mat)
+        ra, ca, rb, cb = t_r_c[1:]
+        ans = uf.is_same(ra*uf.w+ca, rb*uf.w+cb)
 
         if ans:
             print('Yes')
         else:
             print('No')
 
-print(mat)
