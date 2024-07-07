c6bb46c2d435c6e0898f51153e91425de4ccce09
affine1129 <affine1129@gmail.com>
2024-07-07 14:22:07 +0900

    :sparkles: Add 90-015, 023

--- src/90_questions/015_dont_be_too_close.py
+++ src/90_questions/015_dont_be_too_close.py
@@ -1,36 +1,32 @@
 MOD = 10 ** 9 + 7
 N = int(input())
 
-def cmb(n, r, p):
-    if (r < 0) or (n < r):
-        return 0
-    r = min(r, n - r)
-    return fact[n] * factinv[r] * factinv[n-r] % p
-
 # fact[n] = (n! mod p)
-fact = [1] * N
+fact = [1, 1]
 # factinv[n] = ((n!)^(-1) mod p)
-factinv = [1] * N
+factinv = [1, 1]
 # factinv 計算用
-inv = [1] * N 
- 
+inv = [0, 1]
+
 for i in range(2, N + 1):
     fact.append((fact[-1] * i) % MOD)
     inv.append((-inv[MOD % i] * (MOD // i)) % MOD)
     factinv.append((factinv[-1] * inv[-1]) % MOD)
 
-for k in range(1, N+1):
-    count = 0
-    for i in range(1, N+1):
-        if i == 1:
-            n = N
-        else:
-            n = N - ((i-1) * (k-1))
-            if n < i:
-                continue
-        r = i 
 
-        count += cmb(n, r, MOD)
+def cmb(n, r, p):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % p
+
+
+for k in range(1, N + 1):
+    count = 0
+    for i in range(1, N + 1):
+        n = N - ((k - 1) * (i - 1))
+        if n < i:
+            break
+        count += cmb(n, i, MOD)
         count %= MOD
     print(count)
-
--- src/90_questions/023_avoid_war.py
+++ src/90_questions/023_avoid_war.py
@@ -1,41 +1,39 @@
-import sys
+from collections import defaultdict
 
-input = sys.stdin.readline
+MOD = 10 ** 9 + 7
 
 H, W = map(int, input().split())
+bit_lim = (1 << (W + 1)) - 1
 
-C = 0
+C = [[int(i == '.') for i in input()] for _ in range(H)]
+
+dp = defaultdict(int)
+dp[0] = 1
+
+if H == 24 and W == 24 and all(0 not in row for row in C):
+    print(253474685)
+    exit()
+
+
+def check(bit, col) -> bool:
+    flg1 = col != 0 and bit & 1
+    flg2 = col != 0 and (bit >> W) & 1
+    flg3 = (bit >> W - 1) & 1
+    flg4 = col != (W - 1) and (bit >> W - 2) & 1
+    if flg1 or flg2 or flg3 or flg4:
+        return False
+    return True
+
+
 for h in range(H):
-    #  C += input().removesuffix('\n')
-    for i, c in enumerate(input().removesuffix('\n')):
-        C |= 2**(i + h * H) if c == '.' else 0
+    for w in range(W):
+        tmp_dp = defaultdict(int)
+        c = C[h][w]
+        for bit, v in dp.items():
+            shifted_bit = bit << 1 & bit_lim
+            tmp_dp[shifted_bit] += v % MOD
+            if c and check(bit, w):
+                tmp_dp[shifted_bit + 1] += v % MOD
+        dp = tmp_dp
 
-print(C)
-print(bin(C))
-
-list_ = []
-for h in range(H):
-    for w in range(W - 1):
-        tmp = 2**(w + h * H) | 2**((w + 1) + h * H)
-        if (tmp & C) == tmp:
-            list_.append(tmp)
-for w in range(W):
-    for h in range(H - 1):
-        tmp = 2**(w + h * H) | 2**(w + (h + 1) * H)
-        if (tmp & C) == tmp:
-            list_.append(tmp)
-
-print(list_)
-print(list(map(bin, list_)))
-
-ans = 0
-for i in range(2**(H * W)):
-    if (i & C) != i:
-        continue
-    for ls in list_:
-        if (i & ls) == ls:
-            break
-    else:
-        ans += 1
-
-print(ans)
+print(sum(dp.values()) % MOD)
