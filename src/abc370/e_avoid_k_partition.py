1333244959bd18b4f813c403293355589b59e1a1
affine1129 <affine1129@gmail.com>
2024-09-23 11:27:31 +0900

    :+1:Update abc370-e

--- src/abc370/e_avoid_k_partition.py
+++ src/abc370/e_avoid_k_partition.py
@@ -1,30 +1,18 @@
+from collections import defaultdict
+
 MOD = 998244353
 
 N, K = map(int, input().split())
-A = list(map(int, input().split()))
+A = map(int, input().split())
 
-ans = 1
-for _ in range(N - 1):
-    ans *= 2
-    ans %= MOD
+m = defaultdict(int)
 
-print(ans)
+dp = all = m[0] = 1
+acc = 0
+for a in A:
+    acc += a
+    dp = (all - m[acc - K]) % MOD
+    all += dp
+    m[acc] += dp
 
-head = 0
-tail = 0
-
-sum_ = A[0]
-exc = 0
-while head != (N - 1) or tail != (N - 1):
-    if sum_ <= K and head != (N - 1):
-        head += 1
-        sum_ += A[head]
-    elif sum_ >= K:
-        sum_ -= A[tail]
-        tail += 1
-
-    exc += bool(sum_ == K)
-    exc %= MOD
-    print(head, tail)
-
-print(ans - exc + (MOD if ans < exc else 0))
+print(dp)
